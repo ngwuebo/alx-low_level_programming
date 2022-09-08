f85be489d67b5d6ef73586b7f0f84037bb5cbbad
ngwuebo <nnathaniel155@gmail.com>
2022-09-08 16:39:11 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * Main - print the string in the put function
  *
  * Description: using the main function
- * this programme prints "Programming is like building a multilingual puzzle
+ * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
