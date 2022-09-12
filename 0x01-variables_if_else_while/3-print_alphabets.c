1fbf73df7964e11c9d56a3101c4c27b141292f46
ngwuebo <nnathaniel155@gmail.com>
2022-09-12 16:04:50 -0700

    work

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,17 @@
 #include <stdio.h>
+
 /**
- *
- * main - lowercase alphabet
+ * main - lowercase alphabets
  *
  * Return: Always (Success)
-      */
-
- int main(void)
-
+ */
+int main(void)
 {
+	char c;
 
-         char c;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
 
-
-
-         for (c = 'a'; c <= 'z'; c++)
-
-		
-                   putchar(c);
-
-
-
-             	 putchar('\n');
-
-        return (0);
-
+	putchar('\n');
+	return (0);
 }
