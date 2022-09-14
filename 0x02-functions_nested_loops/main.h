e9d1491c66795a4959eb88b2e12f7c6ccb4bea2f
ngwuebo <nnathaniel155@gmail.com>
2022-09-14 14:39:18 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,7 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
-
-
-int _putchar(
+int _putchar(char)
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
@@ -16,3 +14,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
 
+#endif
